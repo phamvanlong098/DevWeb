7e08252a928fe7ffdb5be916dbf038191782887b
unknown <phamvanlong098@gmail.com>
2021-12-21 10:29:50 +0700

    fixing taikhoan.js

--- src/routes/taikhoan.js
+++ src/routes/taikhoan.js
@@ -25,4 +25,74 @@ router.get('/capcon', (req, res) => {
 })
 
 
+// session
+router.get('/', (req, res, next) => {
+    res.redirect('/taikhoan/login')
+})
+
+router.get('/login', (req, res, next) => {
+    res.render('special/login')
+})
+
+router.post('/login', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+})
+
+
+// router.post('/logout', (req, res, next) => {
+//     req.session.destroy()
+//     res.status(300).redirect('/')
+// })
+
+
+// function checklogin(req, res, next) {
+//     if(req.session.data) {
+//         next()
+//     }
+//     else {
+//         res.status(300).redirect('/login')
+//     }
+// }
+
+// function checkStudent(req, res, next) {
+//     const role = req.session.data.role;
+//     if(role === 'student' || role === 'teacher' || role === 'manager') {
+//         next()
+//     }
+//     else{
+//         res.send('NO PERMITTION')
+//     }
+// }
+// function checkTeacher(req, res, next) {
+//     const role = req.session.data.role;
+//     if(role === 'teacher' || role === 'manager') {
+//         next()
+//     }
+//     else{
+//         res.send('NO PERMITTION')
+//     }
+// }
+// function checkManager(req, res, next) {
+//     const role = req.session.data.role;
+//     if(role === 'manager') {
+//         next()
+//     }
+//     else{
+//         res.send('NO PERMITTION')
+//     }
+// }
+
+// router.get('/student', checklogin, checkStudent, (req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'logout.html'))
+// })
+// router.get('/teacher', checklogin, checkTeacher, (req, res, next) => {
+//     res.send('teacher')
+// })
+// router.get('/manager', checklogin, checkManager, (req, res, next) => {
+//     res.send('manager')
+// })
+
+
 module.exports = router
